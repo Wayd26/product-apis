5bd59fa3847d0600cf51144703ad95b6a86c97a9
Wayd26 <wayedoun@gmail.com>
2023-01-13 12:16:47 +0100

    add login method to user controller

--- controllers/user.js
+++ controllers/user.js
@@ -23,5 +23,30 @@ exports.signup = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-
+    User.findOne({email: req.body.email}).then(
+        (user) => {
+            if(!user) {
+                return res.status(401).json({message: 'User credentials invalid'})
+            }
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if(!valid) {
+                        return res.status(401).json({message: 'User credentials invalid'})
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    })
+                } 
+            ).catch(
+                (error) => {
+                    res.status(500).json({error: error})
+                }
+            )
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({error: error})
+        }
+    )
 }
